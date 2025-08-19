97e781ddcd5c17ad54d63f043bfeb8a4db8223e7
Rahulsonale <rahulsonale002@gmail.com>
2025-08-19 21:05:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 // your code here
+let inp1 = document.querySelector("#name");
+let inp2 = document.querySelector("#year");
+let h3 = document.querySelector("#url");
+let submit = document.querySelector("#button");
+
+submit.addEventListener("click", function (e) {
+    e.preventDefault();
+  h3.textContent = `https://localhost:8080/${inp1.value}&${inp2.value}`;
+});
