26293514730732cded81059dbd0af36fe2cbb6f6
Rahulsonale <rahulsonale002@gmail.com>
2025-08-19 21:08:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
-// your code here
-let inp1 = document.querySelector("#name");
-let inp2 = document.querySelector("#year");
-let h3 = document.querySelector("#url");
-let submit = document.querySelector("#button");
+document.addEventListener("DOMContentLoaded", () => {
+  let inp1 = document.querySelector("#name");
+  let inp2 = document.querySelector("#year");
+  let h3 = document.querySelector("#url");
+  let submit = document.querySelector("#button");
 
-submit.addEventListener("click", function (e) {
+  submit.addEventListener("click", function (e) {
     e.preventDefault();
-  h3.textContent = `https://localhost:8080/${inp1.value}&${inp2.value}`;
+
+    let params = [];
+    if (inp1.value) params.push(`name=${inp1.value}`);
+    if (inp2.value) params.push(`year=${inp2.value}`);
+
+    let url = "https://localhost:8080/";
+    if (params.length > 0) {
+      url += "?" + params.join("&");
+    }
+
+    h3.textContent = url;
+  });
 });
